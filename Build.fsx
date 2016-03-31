09f071871058dd32e813b8920b0d2f49180bdc70
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-03-31 14:33:20 +0100

    Update to fake script and also added a simple bat file for running the script

--- Build.fsx
+++ Build.fsx
@@ -1,47 +1,147 @@
-﻿#r @"packages/FAKE.3.35.4/tools/FakeLib.dll"
+﻿#r @"tools/FAKE/tools/FakeLib.dll"
+#r @"tools/FAKE/tools/Fake.IIS.dll"
+#r @"tools/FAKE/tools/Microsoft.Web.Administration.dll"
 
 open Fake
 
+open Fake.IISHelper
+
+let findNuget = @"tools/nuget"
+
 RestorePackages()
 
-let projectName = "MyProject"
-let folderPrecompiled = @"\..\"+ projectName + ".Release_precompiled "
-let publishingProfile = "PublishForViewCompile"
-let nUnitToolPath = currentDirectory @@ @"packages\NUnit.Runners.2.6.4\tools\"
 
-let publishDirectory = getBuildParamOrDefault "publishDirectory"  @"D:\CompiledSource" @@ projectName
+let nUnitToolPath = @"tools\NUnit.ConsoleRunner\tools\nunit3-console.exe"
+let rootPublishDirectory = getBuildParamOrDefault "publishDirectory"  @"C:\CompiledSource"
 let testDirectory = getBuildParamOrDefault "buildMode" "Debug"
 let myBuildConfig = if testDirectory = "Release" then MSBuildRelease else MSBuildDebug
+let userPath = getBuildParamOrDefault "userDirectory" @"C:\Users\buildguest\"
 
+let isAutomationProject = getBuildParamOrDefault "AcceptanceTests" "false"
+
+let mutable projectName = ""
+let mutable folderPrecompiled = @"\"+ projectName + ".Release_precompiled "
+let mutable publishDirectory = rootPublishDirectory @@ projectName
+let mutable publishingProfile = projectName + "PublishProfile"
+
+Target "Set Solution Name" (fun _ ->
+    
+    let directoryHelper = FileSystemHelper.directoryInfo(currentDirectory).Name
+
+    let mutable solutionNameToMatch = ""
+    if isAutomationProject.ToLower() = "false" then 
+        solutionNameToMatch <- "*.sln" 
+    else 
+        solutionNameToMatch <- "*Automation.sln"
+
+    let findSolutionFile = FindFirstMatchingFile "*.sln" currentDirectory
+    
+    let solutionFileHelper = FileSystemHelper.fileInfo(findSolutionFile)
+            
+    projectName <- solutionFileHelper.Name.Replace(solutionFileHelper.Extension, "")
+    folderPrecompiled <- @"\"+ projectName + ".Release_precompiled "
+    publishDirectory <- rootPublishDirectory @@  projectName
+    publishingProfile <- projectName + "PublishProfile"
+
+
+    trace publishingProfile
+    trace publishDirectory
+    trace folderPrecompiled
+
+    trace ("Project Name has been set to: " + projectName)
+
+)
 
 Target "Clean Publish Directory" (fun _ ->
     trace "Clean Publish Directory"
+
+    if FileHelper.TestDir(rootPublishDirectory) then
+        FileHelper.CleanDir(rootPublishDirectory)
+    else
+        FileHelper.CreateDir(rootPublishDirectory)
+
+
+    if FileHelper.TestDir(publishDirectory) then
+        FileHelper.CleanDir(publishDirectory)
+    else
+        FileHelper.CreateDir(publishDirectory)
+    
 
     if FileHelper.TestDir(publishDirectory) then
         FileHelper.CleanDir(publishDirectory)
     else
         FileHelper.CreateDir(publishDirectory)
 
-    
-    let directoryinfo = FileSystemHelper.directoryInfo(EnvironmentHelper.combinePaths publishDirectory folderPrecompiled)
+    let directoryinfo = FileSystemHelper.directoryInfo(EnvironmentHelper.combinePaths publishDirectory @"\..\"+ projectName + ".Release_precompiled ")
     let directory = directoryinfo.FullName
     trace directory
     if FileHelper.TestDir(directory) then
         FileHelper.CleanDir(directory)
+    else
+        FileHelper.CreateDir(directory)
+
 )
 
+Target "Build DNX Project"(fun _ ->
+
+    trace "Build DNX PRoject"
+    let dnuDir = userPath @@ @"\.dnx\runtimes\dnx-clr-win-x86.1.0.0-rc1-update1\bin\dnu.cmd"
+    trace dnuDir
+        
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- dnuDir
+            info.Arguments <- @"publish .\src\MvcPOC --out .\WR\Publish --configuration Release --runtime dnx-clr-win-x86.1.0.0-rc1-update1"
+            info.WorkingDirectory <- rootPublishDirectory @@ @"..\..\"
+        ) (System.TimeSpan.FromMinutes 10.)
+        
+    if result <> 0 then failwith "Failed to build DNX project"
+
+)
+
+let buildSolution() = 
+    let buildMode = getBuildParamOrDefault "buildMode" "Debug"
+
+    let properties = 
+                    [
+                        ("TargetProfile","cloud")
+                    ]
+
+    !! (@"./" + projectName + ".sln")
+        |> MSBuildReleaseExt null properties "Publish"
+        |> Log "Build-Output: "
+
+
+Target "Build Acceptance Solution"(fun _ ->
+    buildSolution()
+)
+
+
+
 Target "Build Solution"(fun _ ->
+    buildSolution()
+)
+
+
+
+
+Target "Publish Solution"(fun _ ->
     
     let buildMode = getBuildParamOrDefault "buildMode" "Debug"
+
+
+    let directoryinfo = FileSystemHelper.directoryInfo(@".\" @@ publishDirectory)
+    let directory = directoryinfo.FullName
+    trace directory
 
     let properties = 
                     [
                         ("DebugSymbols", "False");
                         ("Configuration", buildMode);
                         ("PublishProfile", @".\" + publishingProfile + ".pubxml");
-                        ("PublishUrl",publishDirectory);
+                        ("PublishUrl", directory);
                         ("DeployOnBuild","True");
-                        ("ToolsVersion","12");
+                        ("ToolsVersion","14");
                     ]
 
     !! (@"./" + projectName + ".sln")
@@ -71,16 +171,16 @@ Target "Run NUnit Tests" (fun _ ->
 
 
     trace "Run NUnit Tests"
-    let testDlls = !! ("./**/bin/" + testDirectory + "/*.UnitTests.dll") 
-    
+    let testDlls = !! ("./*.UnitTests/bin/" + testDirectory + "/*.UnitTests.dll") 
+
     
     for testDll in testDlls do
-        [testDll] |> NUnit (fun p ->
+        [testDll] |> Fake.Testing.NUnit3.NUnit3 (fun p ->
             {p with
-                DisableShadowCopy = true;
                 ToolPath = nUnitToolPath;
-                Framework = "net-4.0";
-                OutputFile = fileInfo(testDll).DirectoryName @@ "TestResult.xml"
+                ShadowCopy = false;
+                Framework = Testing.NUnit3.NUnit3Runtime.Net45;
+                ResultSpecs = ["TestResult.xml;format=nunit2"];
                 })
 )
 
@@ -104,36 +204,41 @@ Target "Building Integration Tests" (fun _ ->
 
 Target "Run Integration Tests" (fun _ ->
 
-
     trace "Run Integration Tests"
     let testDlls = !! ("./**/bin/" + testDirectory + "/*.IntegrationTests.dll") 
-    
-    
+        
     for testDll in testDlls do
-        [testDll] |> NUnit (fun p ->
+        [testDll] |> Fake.Testing.NUnit3.NUnit3 (fun p ->
             {p with
-                DisableShadowCopy = true;
                 ToolPath = nUnitToolPath;
-                IncludeCategory = "Integration";
-                Framework = "net-4.5";
-                OutputFile = fileInfo(testDll).DirectoryName @@ "TestResult.xml"
+                StopOnError = false;
+                //OutputDir = fileInfo(testDll).DirectoryName;
+                ResultSpecs = ["TestResult.xml;format=nunit2"];
                 })
 )
 
 Target "Compile Views" (fun _ ->
     trace "Compiling views"
     
-    let directoryinfo = FileSystemHelper.directoryInfo(EnvironmentHelper.combinePaths publishDirectory (@"\..\" + folderPrecompiled))
+    let directoryinfo = FileSystemHelper.directoryInfo(@".\" @@ publishDirectory)
     let directory = directoryinfo.FullName
     trace directory
+
+
+    let directoryinfo = FileSystemHelper.directoryInfo(publishDirectory @@ @"\..\"+ folderPrecompiled)
+    let directoryOutput = directoryinfo.FullName
+    trace directoryOutput
+
 
     let result =
         ExecProcess (fun info ->
             info.FileName <- ("C:/Windows/Microsoft.NET/Framework/v4.0.30319/aspnet_compiler.exe")
-            info.Arguments <- @"-v \" + folderPrecompiled + " -p . " + directory
+            info.Arguments <- @"-v \" + folderPrecompiled + " -p . " + directoryOutput
             info.WorkingDirectory <- publishDirectory
         ) (System.TimeSpan.FromMinutes 10.)
         
+
+
     if result <> 0 then failwith "Failed to compile views"
 )
 
@@ -172,18 +277,53 @@ Target "Zip Compiled Source" (fun _ ->
 
 )
 
-"Cleaning Integration Tests"
+
+let CreateSite (environment, directory, port) =
+    
+    let siteName = (sprintf "%s.%s.WebSite" projectName environment)
+    let appPoolName = (sprintf "%s.%s.appPool" projectName environment)
+    let port = port
+    let path =  directory
+
+    let siteConfig = SiteConfig(siteName, port, path, appPoolName)
+    let appPoolConfig = ApplicationPoolConfig(appPoolName)
+
+    (IIS
+      (Site siteConfig)
+      (ApplicationPool appPoolConfig)
+      (None))
+
+Target "Create Accceptance Test Site in IIS" (fun _ ->
+    trace "Create Acceptance site"
+    let directoryinfo = FileSystemHelper.directoryInfo(publishDirectory)
+    let directory = directoryinfo.FullName
+    CreateSite("Acc",directory,":5050:")
+
+)
+
+Target "Create Development Site in IIS" (fun _ ->
+    trace "Create Development Site"
+    let directory = FileSystemHelper.directoryInfo(currentDirectory).FullName @@ projectName
+    CreateSite("Dev",directory,":7070:")
+)
+
+"Set Solution Name"
+    ==>"Build Acceptance Solution"
+    ==>"Cleaning Integration Tests"
     ==>"Building Integration Tests"
-    ==>"Run Integration Tests"
+    //==>"Run Integration Tests"
 
-
-"Clean Publish Directory"
+"Set Solution Name"
+   ==>"Clean Publish Directory"
    ==>"Build Solution"
+   ==>"Publish Solution"
    ==>"Cleaning Unit Tests"
    ==>"Building Unit Tests"
    ==>"Run NUnit Tests"
    ==>"Compile Views"
    ==>"Zip Compiled Source"
+   ==>"Create Development Site in IIS"
+   ==>"Create Accceptance Test Site in IIS"
 
-
-RunTargetOrDefault  "Zip Compiled Source"
+   
+RunTargetOrDefault  "Create Accceptance Test Site in IIS"
