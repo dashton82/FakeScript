40aa449b58b9aa558c128ad7f20763b032914557
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-01-31 09:40:04 +0000

    Move solution and rename

---
+++ Fake.TestProject/Fake.TestProject.Web.UnitTests/WhenTesting.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Fake.TestProject.Web.UnitTests
+{
+    [TestFixture]
+    public class WhenTesting
+    {
+        [Test]
+        public void ThenTrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Fake.TestProject/Fake.TestProject.Web.XUnitTests/WhenTesting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fake.TestProject.Web.XUnitTests
+{
+    public class WhenTesting
+    {
+        [Fact]
+        public void ThenTrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
